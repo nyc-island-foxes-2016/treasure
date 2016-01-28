a94fba4b13e65ad6dd9ffa63db784a339afe2cc2
Carolina Medellin & Darius & Nicola Beuscher & Natalie Polen <pair+caromedellin.datmar.deweydell.nataliecodes@devbootcamp.com>
2016-01-28 17:03:47 -0500

    Write users table migration file

--- db/migrate/20160128211900_create_users.rb
+++ db/migrate/20160128211900_create_users.rb
@@ -1,7 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      #TODO
+      t.string :name, null: false, limit: 30
+      t.string :email, null: false, unique: true
+      t.string :password_digest, null: false
 
       t.timestamps null: false
     end
