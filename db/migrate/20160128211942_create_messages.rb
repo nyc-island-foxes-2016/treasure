0d9fdf753394801ecac498c955e482524bc4176b
Carolina Medellin & Darius & Nicola Beuscher & Natalie Polen <pair+caromedellin.datmar.deweydell.nataliecodes@devbootcamp.com>
2016-01-28 17:29:02 -0500

    Add content to message migration

--- db/migrate/20160128211942_create_messages.rb
+++ db/migrate/20160128211942_create_messages.rb
@@ -2,7 +2,7 @@ class CreateMessages < ActiveRecord::Migration
   def change
     create_table :messages do |t|
       t.integer :match_id, index: true, null: false
-
+      t.text    :content, null: false, limit: 400
       t.timestamps null: false
     end
   end
