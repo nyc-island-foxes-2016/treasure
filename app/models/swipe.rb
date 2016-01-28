928d62d1ef22f4fd3ca32d78f2b9df3f9fc5ff1e
Carolina Medellin & Darius & Nicola Beuscher & Natalie Polen <pair+caromedellin.datmar.deweydell.nataliecodes@devbootcamp.com>
2016-01-28 18:27:40 -0500

    Schema file added, swipe model created

---
+++ app/models/swipe.rb
@@ -0,0 +1,11 @@
+class Swipe < ActiveRecord::Base
+  validates :owned_item_id, presence: true
+  validates :judged_item_id, presence: true
+  validates :direction, presence: true
+
+  belongs_to :owned_item, foreign_key: :owned_item_id, class_name: "Item"
+  belongs_to :judged_item, foreign_key: :judged_item_id, class_name: "Item"
+  has_many :first_match_swipes, foreign_key: :swipe_1_id, class_name: "Match"
+  has_many :second_match_swipes, foreign_key: :swipe_2_id, class_name: "Match"
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,13 @@ class User < ActiveRecord::Base
   has_secure_password
 
   # Paperclip code
-  attr_accessible :avatar
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  # attr_accessible :avatar
+  # has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  # validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
   has_many :items
-  has_many :swipes, through: :items #TODO: verify
-  has_many :matches, through: :swipes  #TODO: verify
+  has_many :given_swipes, through: :items
+  has_many :received_swipes, through: :items
+  has_many :matches, through: :swipes #TODO: verify
   has_many :messages
 end
