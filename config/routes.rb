8d91f724f4f8a47fb8a88b41bbc93dda8bbbafdb
caromedellin <caromedellin@gmail.com>
2016-01-28 22:27:48 -0500

    add basic routes only user, session and index for items

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  root to: 'items#index'
+  resources :sessions, only: [:new, :create, :destroy]
+  delete 'session' => 'sessions#destroy'
+
+  resources :users, only: [:new, :create, :show]
+  resources :items, only: [:index]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
