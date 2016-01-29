49f1561c582372dfecd121c73fea2dd6e998a539
Carolina Medellin & Darius & Nicola Beuscher & Natalie Polen <pair+caromedellin.datmar.deweydell.nataliecodes@devbootcamp.com>
2016-01-29 12:11:47 -0500

    Work on associations for item, match, and swipe

--- app/models/swipe.rb
+++ app/models/swipe.rb
@@ -5,7 +5,15 @@ class Swipe < ActiveRecord::Base
 
   belongs_to :owned_item, foreign_key: :owned_item_id, class_name: "Item"
   belongs_to :judged_item, foreign_key: :judged_item_id, class_name: "Item"
-  has_many :first_match_swipes, foreign_key: :swipe1_id, class_name: "Match"
-  has_many :second_match_swipes, foreign_key: :swipe2_id, class_name: "Match"
+  has_many :proposed_swipe_matches, foreign_key: :swipe1_id, class_name: "Match"
+  has_many :accepted_swipe_matches, foreign_key: :swipe2_id, class_name: "Match"
+
+  after_save :make_match
+
+  def make_match
+    return unless direction == "R"
+    first_swipe = Swipe.find_by(judged_item: self.owned_item, owned_item: self.judged_item, direction: "R")
+    Match.create!(first_swipe: first_swipe, second_swipe: self) if first_swipe
+  end
 
 end
