053a779f5699014fa50dec528f1df4be2bb5380a
Darius & Natalie Polen <pair+datmar.nataliecodes@devbootcamp.com>
2016-01-29 16:56:40 -0500

    Implement items#create route and view

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,21 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    @item.user = current_user
+    if @item.save
+      redirect_to item_path
+    else
+      flash[:notice] = "We could not save your treasure. Try again, matey!"
+      redirect_to new_item_path
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description)
+  end
 end
