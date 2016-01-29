e4192829183dd15aeec6d2fde3175ab28157993b
caromedellin <caromedellin@gmail.com>
2016-01-29 16:51:49 -0500

    Add users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      respond_to do |format|
+        format.json {render json: @user}
+        format.html {redirect_to @user}
+      end
+    else
+      render :new
+    end
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+end
