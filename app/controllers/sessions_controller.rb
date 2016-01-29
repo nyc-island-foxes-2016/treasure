947b6bf62e5361be1c8e3d5dccaebc0efb838aa8
Carolina Medellin & Darius <pair+caromedellin.datmar@devbootcamp.com>
2016-01-29 13:07:50 -0500

    Add oauth_create route for facebook auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,6 +19,14 @@ class SessionsController < ApplicationController
     end
   end
 
+  def oauth_create
+    u = User.create_or_get_from_oauth(request.env['omniauth.auth'])
+    if u
+      flash[:notice] = 'Login successful'
+      redirect_to root_path
+    end
+  end
+
   def destroy
     session.clear
     redirect_to root_path
