ebf6ba4dbf4935635d66ab7b132a92a93b42c0e5
Carolina Medellin & Darius <pair+caromedellin.datmar@devbootcamp.com>
2016-01-29 11:51:15 -0500

    Add sessions_controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+  def new
+    @user = User.new
+    if request.xhr?
+      render '_form', layout: false, locals: {user: @user}
+    end
+  end
+
+  def create
+    @user = User.find_by(email: params[:email], provider: 'SELF')
+    if @user and @user.authenticate params[:password]
+      session[:user_id] = @user.id
+      respond_to do |format|
+        format.json {render json: @user}
+        format.html {redirect_to @user}
+      end
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to root_path
+  end
+end
