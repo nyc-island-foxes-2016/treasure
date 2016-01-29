5d45b4f5516008ae5fd8717e37c156d195ef8716
caromedellin <caromedellin@gmail.com>
2016-01-29 18:38:30 -0500

    add paperclip' '

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ActiveRecord::Base
   has_secure_password
 
   # Paperclip code
-  # attr_accessible :avatar
-  # has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  # validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
   has_many :items
   has_many :given_swipes, through: :items
