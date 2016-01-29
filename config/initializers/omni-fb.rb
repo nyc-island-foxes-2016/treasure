43ae1813e495a2f93b9f28ab084bc9f297e1b892
Carolina Medellin & Darius <pair+caromedellin.datmar@devbootcamp.com>
2016-01-29 13:10:23 -0500

    Remove all params from facebook scope auth for now

--- config/initializers/omni-fb.rb
+++ config/initializers/omni-fb.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
-           :scope => 'email,user_birthday,read_stream', :display => 'popup'
+           :scope => '', :display => 'popup'
+           #TODO: Add appropriate scope
 end
