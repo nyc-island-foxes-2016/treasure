d0ce20fcfd17f8db2a0ee3b84b8d608808ec5ccb
Darius <email": null>
2016-01-30 13:51:53 -0500

    Change make_match method so that it is called explicitly in the controller, not after swipe save

--- app/controllers/swipes_controller.rb
+++ app/controllers/swipes_controller.rb
@@ -3,6 +3,10 @@ class SwipesController < ApplicationController
     @swipe = Swipe.new(swipe_params)
     @swipe.my_item = current_user.items.first
     if @swipe.save
+      if @swipe.make_match
+
+        binding.pry
+      end
       redirect_to available_items_path
     else
       flash[:notice] = "Something went wrong. Sorry!"
