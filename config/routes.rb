0e29c15f4be753e925b734e0f68f32648e5e0690
Nicola Beuscher <email": null>
2016-01-30 15:19:37 -0500

    Add edit button for items and edit and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
 
 
-  resources :items, only: [:index, :new, :create, :show] do
+  resources :items, only: [:index, :new, :create, :show, :edit, :update] do
     resources :matches, only: [:index, :show]
   end
 
