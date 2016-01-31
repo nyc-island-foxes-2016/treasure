8981dc0f6c115818e8731fabb59e73954bf2505a
caromedellin <caromedellin@gmail.com>
2016-01-31 03:08:16 -0500

    Add access to other item for matches show

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -3,6 +3,7 @@ class MatchesController < ApplicationController
   def show
     @match = Match.includes(:messages).find(params[:id])
     @my_item =@match.my_item
+    @other_item =@match.other_item
     @message = Message.new
   end
 end
