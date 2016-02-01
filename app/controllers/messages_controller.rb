bebc3920730d839450bbb11940d85d6d8f87564d
caromedellin <caromedellin@gmail.com>
2016-02-01 18:29:21 -0500

    Change controller to add xhr request option

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,22 +10,13 @@ class MessagesController < ApplicationController
       @match.touch
 
       if request.xhr?
-         # get necessary variables to nest a json
-          puts @message.to_json
-
-          respond_to do |format|
-            format.json {render json: @message}
-
-          # format.json {render json: {message: @message, user: @user}}
-          end
+        @all_messages = @match.messages
+        @all_users = @all_messages.map { |message| user= User.find_by(id: message.user_id) }
+        @user = @message.user
+          puts @match.messages.to_json
+          render :create, layout: false
         else
-
-
-
-
-      
-      redirect_to item_match_path(@my_item, @match)
-
+          redirect_to item_match_path(@my_item, @match)
       end
     else
       flash[:notice] = "Sorry! We couldn't send your message, phone companies suck"
