e96d098f53eb80ae1ed18f3ee524f59981310d6e
Nicola Beuscher & Natalie Polen <pair+deweydell.nataliecodes@devbootcamp.com>
2016-02-03 10:03:11 -0500

    Remove magic numbers by creating next swipe id constants

--- app/assets/javascripts/swipe.js
+++ app/assets/javascripts/swipe.js
@@ -6,6 +6,9 @@ $(document).ready(function(){
   var currentObject;
   var matchResponse;
 
+  const NEXT_LEFT_SWIPE_ID = $(".swipe-action input")[3];
+  const NEXT_RIGHT_SWIPE_ID = $(".swipe-action input")[8];
+
 // Handle left or right swipes via clicking the respective buttons
     $(".swipe-action").on("submit", function(event) {
       event.preventDefault();
@@ -23,8 +26,8 @@ $(document).ready(function(){
           dataType: "JSON"
         }).done(function(secondResponse){
             nextItemObject = secondResponse;
-            $(".swipe-action input")[3].value = nextItemObject.id;
-            $(".swipe-action input")[8].value = nextItemObject.id;
+            NEXT_LEFT_SWIPE_ID.value = nextItemObject.id;
+            NEXT_RIGHT_SWIPE_ID.value = nextItemObject.id;
         }).then(function(event) {
           if (matchResponse.match){
             currentItemName = $("#item-container h3").html()
@@ -72,8 +75,8 @@ $(document).ready(function(){
             dataType: "JSON"
           }).done(function(secondResponse){
             nextItemObject = secondResponse;
-            $(".swipe-action input")[3].value = nextItemObject.id;
-            $(".swipe-action input")[8].value = nextItemObject.id;
+            NEXT_LEFT_SWIPE_ID.value = nextItemObject.id;
+            NEXT_RIGHT_SWIPE_ID.value = nextItemObject.id;
           }).then(function(event) {
             if (nextItemObject.message != "No More Items") {
               $( "#item-container" ).animate({ marginRight: "15em", opacity: 0 },
@@ -101,8 +104,8 @@ $(document).ready(function(){
             dataType: "JSON"
           }).done(function(response){
             nextItemObject = response;
-            $(".swipe-action input")[3].value = nextItemObject.id;
-            $(".swipe-action input")[8].value = nextItemObject.id;
+            NEXT_LEFT_SWIPE_ID.value = nextItemObject.id;
+            NEXT_RIGHT_SWIPE_ID.value = nextItemObject.id;
           }).then(function(event) {
             if (matchResponse.match){
               currentItemName = $("#item-container h3").html()
