3b72b2cad2617de089ed0bf93333271f8ff94b99
Darius Atmar <dariusatmar@Dariuss-MacBook-Pro-2.local>
2016-02-03 10:01:34 -0500

    Make message touch its match

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,6 @@ class Message < ActiveRecord::Base
   validates :content, presence: true, length: {minimum: 1, maximum: 400}
   validates :match_id, :user_id, presence: true
 
-  belongs_to :match
+  belongs_to :match, touch: true
   belongs_to :user
 end
