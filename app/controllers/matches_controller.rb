5013097887a9800b29944a5fad25de9ebd8d0aff
caromedellin <caromedellin@gmail.com>
2016-02-04 01:50:12 -0500

    Add query necessary for ajax request

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -3,6 +3,7 @@ class MatchesController < ApplicationController
   def show
     @my_item = Item.find_by(id: params[:item_id])
     @match = Match.includes(:messages).find(params[:id])
+    @all_messages = @match.messages
     if @my_item && @my_item.user == current_user && @match.belongs_to_item?(@my_item)
       @other_item = @match.other(@my_item)
       @message = Message.new
